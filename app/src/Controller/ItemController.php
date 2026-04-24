49807a2e3c7212ec3921d1ff1ad39db558c348a9
CamileGhastine <camileghastine@hotmail.com>
2026-04-24 16:29:33 +0200

    fix bug

--- app/src/Controller/ItemController.php
+++ app/src/Controller/ItemController.php
@@ -36,12 +36,10 @@ final class ItemController extends AbstractController
     }
 
     #[Route('/item/{id<[0-9]+>}', name: 'app_item_show')]
-    public function show($id, Request $request, OfferHandler $offerHandler, EmailSender $emailSender): Response
+    public function show($id, Request $request, OfferHandler $offerHandler): Response
     {
         $user = $this->getUser();
         $item = $this->itemRepository->findWithOffers($id);
-
-        $emailSender->sendPaymentLink($item);
 
         $form = $this->createForm(OfferType::class);
         $form->handleRequest($request);
